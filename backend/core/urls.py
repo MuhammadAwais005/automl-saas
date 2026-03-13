2b7df7fc21a13225561cea52c6c360a65743d550
Awais Shakil <your-real-email@example.com>
2026-03-13 05:06:18 +0500

    Fix HTTPS and Media URLs for Production

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,31 +1,14 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-urlpatterns = [
+urlpatterns =[
     path('admin/', admin.site.urls),
     path('api/', include('processor.urls')),
     
@@ -34,5 +17,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refresh Token
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ⚠️ MAGIC FIX: Force Django to serve media files in production (Render)
+urlpatterns +=[
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
